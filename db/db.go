3dab958bc5e7daa6785e785aa1c53341c88de28e
Tomonori HIRATA <hirata@sqlab.jp>
2020-10-12 00:44:22 +0900

    postgres setting

--- db/db.go
+++ db/db.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres" // Use PostgreSQL in gorm
 
@@ -14,7 +16,9 @@ var (
 
 // Init is initialize db from main function
 func Init() {
-	db, err = gorm.Open("postgres", "host=0.0.0.0 port=5432 user=abome dbname=abome password=abome sslmode=disable")
+	// db, err = gorm.Open("postgres", "host=0.0.0.0 port=5432 user=abome dbname=abome password=abome sslmode=disable")
+	databaseURL := os.Getenv("DATABASE_URL") // DATABASE_URL="postgres://abome:abome@0.0.0.0:5432/abome?sslmode=disable"
+	db, err = gorm.Open("postgres", databaseURL)
 	if err != nil {
 		panic(err)
 	}
